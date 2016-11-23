aead4d980f197e3a247c06b848d0fb778fa84f14
Krell <KrellTheHuman@gmail.com>
2016-11-23 00:22:12 -0800

    update view:users

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -12,7 +12,8 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-xs-12 col-md-6 col-md-offset-2">
+        <div class="col-xs-12 col-md-9">
+            <h3 class="m-t-0">Users</h3>
             <ul>
                 <li>user1</li>
                 <li>user2</li>
@@ -20,10 +21,8 @@
                 <li>user4</li>
             </ul>
         </div>
-        <div class="col-xs-12 col-md-2">
-            <div class="col-xs-12">
-                <img src="http://placehold.it/400x400" class="img-responsive">
-            </div>
+        <div class="col-xs-12 col-md-3">
+            <img src="http://placehold.it/400x400" class="img-responsive">
         </div>
     </div>
 @stop
